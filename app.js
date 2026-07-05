a4f2ec55dad85f82436f774fc4ac3cd6d1975646
Ankit Gupta <ankitgupta21j@gmail.com>
2026-07-05 15:37:51 +0100

    test

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ window.addEventListener("message", (event) => {
 
     try {
         const data = JSON.parse(event.data);
+        console.log("EVENT LISTENER", data);
 
         if (data.type === "WA_EMBEDDED_SIGNUP") {
             console.log("Embedded Signup Event:", data);
@@ -55,24 +56,24 @@ function fbLoginCallback(response) {
     console.log("Authorization Code:", code);
 
     // We'll send this code to Apps Script in the next step.
-    const form = new URLSearchParams();
-    form.append("code", code);
+    // const form = new URLSearchParams();
+    // form.append("code", code);
     
-    fetch(BACKEND_URL, {
-        method: "POST",
-        body: form
-    })
-    .then(async (r) => {
-        const text = await r.text();
-        console.log("Raw response:", text);
-        return JSON.parse(text);
-    })
-    .then(data => {
-        console.log(data);
-        document.getElementById("output").textContent =
-            JSON.stringify(data, null, 2);
-    })
-    .catch(console.error);
+    // fetch(BACKEND_URL, {
+    //     method: "POST",
+    //     body: form
+    // })
+    // .then(async (r) => {
+    //     const text = await r.text();
+    //     console.log("Raw response:", text);
+    //     return JSON.parse(text);
+    // })
+    // .then(data => {
+    //     console.log(data);
+    //     document.getElementById("output").textContent =
+    //         JSON.stringify(data, null, 2);
+    // })
+    // .catch(console.error);
 }
 
 document.getElementById("connectBtn").onclick = () => {
@@ -82,8 +83,7 @@ document.getElementById("connectBtn").onclick = () => {
         response_type: "code",
         override_default_response_type: true,
         extras: {
-            featureType: "whatsapp_business_app_onboarding",
-            sessionInfoVersion: "3"
+            setup: {},
         }
     });
 
